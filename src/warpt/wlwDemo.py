1ee377e90832f440d545616719016e0f9535a3bb
Loralee Wheeler <lwheeler@mines.edu>
2014-05-24 11:49:28 -0600

    update getLogs

--- src/warpt/wlwDemo.py
+++ src/warpt/wlwDemo.py
@@ -27,46 +27,31 @@ from warpt import *
 
 ##########################################################################
 wlw = WellLogWarping()
-curve = "v" # well log type. v = velocity, d = density, etc
+curve = "v" # velocity logs
 logs = None
 
 
 perr = 0.25 # power of norm used in alignment error calculation
 ms = 250 # maximum shift
-freplace = 2.0 # value to replace nulls for velocity
+freplace = -2.0 # value to replace nulls for velocity
 fclips = (2.0,6.0) 
 cblabel = "Velocity (km/s)"
-if curve=="d": # settings for density
-  freplace = 1.0
-  fclips = (2.0,2.8)
-  cblabel = "Density (g/cc)"
-if curve=="p": # settings for porosity
-  freplace = 0.0
-  fclips = (0.0,0.45)
-  cblabel = "Porosity"
-if curve=="g": # settings for gamma ray
-  freplace = 30.0
-  fclips = (30.0,160.0)
-  cblabel = "Gamma ray (API)"
 
 ##########################################################################
 
 def main(args):
-  global logs
-  logs = getLogs("d",curve)
+  global logs,sz
+  sz,logs = getLogs(curve)
   goShifts() # simultaneously aligns all logs
   #goWarping() # aligns pairs of logs
   #goErrors() # alignment errors for log pairs
-  #goSort() # plots well locations
 
 """
 Performs simultaneous correlation of many logs. 
 Plots logs before and after alignment.
 """
 def goShifts():
-  sz,fs = resample(logs,curve)
-  # chose a subset of logs below if you do not desire the entire set
-  fs = [fs[0],fs[4],fs[9],fs[14],fs[17],fs[20]] # deepest 6 velocity logs
+  fs = logs
   nk,nl = len(fs[0]),len(fs)
   wlw.setPowError(perr) 
   wlw.setMaxShift(ms)
@@ -107,8 +92,8 @@ Plots log pairs before and after alignment.
 """
 def goWarping():
   # define pairs of logs to compare below
-  pairs = [(0,4),(4,9),(9,14),(14,17),(17,20)] # deepest 6 velocity logs
-  sz,fs = resample(logs,curve)
+  pairs = [(0,1),(1,2),(2,3),(3,4),(4,5)] 
+  fs = logs
   wlw.setPowError(perr)
   wlw.setMaxShift(ms)
   for pair in pairs:
@@ -123,8 +108,27 @@ def goWarping():
     gj = wlw.replaceNulls(gj,freplace)
     fk = wlw.replaceNulls(fk,freplace)
     gk = wlw.replaceNulls(gk,freplace)
+    ii,ff = removeZeros(fi)
+    jj,gg = removeZeros(gj)
+    ki,kf = removeZeros(fk)
+    kj,kg = removeZeros(gk)
+    for ij in range(len(ff)):
+      if ff[ij] == -2.0:
+        ff[ij] = 3.0
+    for ij in range(len(gg)):
+      if gg[ij] == -2.0:
+        gg[ij] = 3.0
+    for ij in range(len(kf)):
+      if kf[ij] == -2.0:
+        kf[ij] = 3.0
+    for ij in range(len(kg)):
+      if kg[ij] == -2.0:
+        kg[ij] = 3.0
+    sff = Sampling(len(ff),sz.getDelta(),sz.getFirst()+ii*sz.getDelta())
+    sgg = Sampling(len(gg),sz.getDelta(),sz.getFirst()+jj*sz.getDelta())
+    skf = Sampling(len(kf),0.5*sz.getDelta(),sz.getFirst()+ki*0.5*sz.getDelta())
+    skg = Sampling(len(kg),0.5*sz.getDelta(),sz.getFirst()+kj*0.5*sz.getDelta())
     title = "("+str(jf)+","+str(jg)+")"
-    sk = Sampling(2*sz.count-1,0.5*sz.delta,sz.first)
     if True:
       sp = SimplePlot()
       sp.setSize(750,500)
@@ -132,9 +136,10 @@ def goWarping():
       sp.setHLabel("Depth (km)")
       sp.setVLabel(cblabel)
       sp.setVLimits(fclips[0],fclips[1])
-      pv = sp.addPoints(sz,fi)
+      sp.setHLimits(0,1.9)
+      pv = sp.addPoints(sff,ff)
       pv.setLineColor(Color.BLACK)
-      pv = sp.addPoints(sz,gj)
+      pv = sp.addPoints(sgg,gg)
       pv.setLineColor(Color.RED)
     if True:
       sp = SimplePlot()
@@ -143,9 +148,10 @@ def goWarping():
       sp.setHLabel("Depth (km)")
       sp.setVLabel(cblabel)
       sp.setVLimits(fclips[0],fclips[1])
-      pv = sp.addPoints(sk,fk)
+      sp.setHLimits(0,1.9)
+      pv = sp.addPoints(skf,kf)
       pv.setLineColor(Color.BLACK)
-      pv = sp.addPoints(sk,gk)
+      pv = sp.addPoints(skg,kg)
       pv.setLineColor(Color.RED)
 
 
@@ -156,13 +162,13 @@ Plots alignment error array with and without an optimal path.
 """
 def goErrors():
   # define pairs of logs to compare below
-  pairs = [(0,4),(4,9),(9,14),(14,17),(17,20)]
-  sz,f = resample(logs,curve)
+  pairs = [(0,1),(1,2),(2,3),(3,4),(4,5)] 
+  fs = logs
   wlw.setPowError(perr)
   wlw.setMaxShift(ms)
   for pair in pairs:
     ia,ib = pair[0],pair[1]
-    e = wlw.computeErrors(f[ia],f[ib])
+    e = wlw.computeErrors(fs[ia],fs[ib])
     wlw.interpolateOddErrors(e)
     nl,nk = len(e[0]),len(e)
     lmax = (nl-1)/2
@@ -179,7 +185,6 @@ def goErrors():
     sp.setVLabel("Lag index l")
     sp.setHFormat("%5f")
     pv = sp.addPixels(sk,sl,transpose(e))
-    #pv.setColorModel(cjet)
     pv.setInterpolation(PixelsView.Interpolation.NEAREST)
     pv.setPercentiles(0,100)
     d = wlw.accumulateErrors(e)
@@ -193,7 +198,6 @@ def goErrors():
     sp.setVLabel("Lag index l")
     sp.setHFormat("%5f")
     pv = sp.addPixels(sk,sl,transpose(e))
-    #pv.setColorModel(cjet)
     pv.setInterpolation(PixelsView.Interpolation.NEAREST)
     pv.setPercentiles(0,100)
     d = wlw.accumulateErrors(e)
@@ -223,141 +227,53 @@ def goErrors():
     pv.setLineColor(Color.WHITE)
     """
 
-"""
-Plots locations of well logs on constant time slice.
-"""
-def goSort():
-  x,y = [],[]
-  for index,log in enumerate(logs):
-    x.append(log.x2[0])
-    y.append(log.x3[0])
-  seis = readImage()
-  nx = len(seis)
-  ny = len(seis[0])
-  dy = 0.025
-  dx = 0.025
-  sx = Sampling(nx,dx,0.0)
-  sy = Sampling(ny,dx,0.0)
-  ss = zerofloat(ny,nx)
-  for ix in range(nx):
-    for iy in range(ny):
-      ss[ix][iy] = seis[ix][iy][500]
-  sp = SimplePlot()
-  sp.addPixels(sy,sx,ss)
-  sp.setSize(700,380)
-  sp.setHLabel("Crossline (km)")
-  sp.setVLabel("Inline (km)")
-  pv = sp.addPoints(x,y)
-  pv.setMarkStyle(PointsView.Mark.FILLED_CIRCLE)
-  pv.setLineStyle(PointsView.Line.NONE)
-  pv.setMarkSize(6)
-  pv.setMarkColor(Color.RED)
-  if curve=="v":
-    x = [x[0],x[4],x[9],x[14],x[17],x[20]]
-    y = [y[0],y[4],y[9],y[14],y[17],y[20]]
-    pv = sp.addPoints(x,y)
-    pv.setLineStyle(PointsView.Line.NONE)
-    pv.setMarkStyle(PointsView.Mark.FILLED_CIRCLE)
-    pv.setMarkSize(10)
-    pv.setMarkColor(Color.RED)
-
 
 #############################################################################
 # utilities
 
-_tpDir = "/data/seis/tpd/"
-_csmDir = _tpDir+"csm/"
-_wellLogsDir = _csmDir+"welllogs/"
-_seismicLogsDir = _csmDir+"seismicz/"
-  
-def getLogs(set,type):
-  fileName = _wellLogsDir+"tpw"+set[0]+".dat"
-  wdata = WellLog.Data.readBinary(fileName)
-  logs = []
-  logt = wdata.getLogsWith(type)
-  for log in logt:
-    logs.append(log)
-  logs = sortLogs(logs)
-  return logs
+def getLogs(type):
+  fileName = type+"logs.txt"
+  ifile = open(fileName,'r+')
+  lines = ifile.readlines()
+  nz = len(lines)
+  ifile.close()
+  ifile = open(fileName,'r+')
+  c = 0
+  start = False
+  while (start != True):
+    line = ifile.readline()
+    c += 1
+    if line == "":
+      print 'End of file'
+      break
+    elif line[0] == '~':
+      start = True
 
-def sortLogs(logs):
-  nlog = len(logs)
-  xlog = zerodouble(nlog)
-  ylog = zerodouble(nlog)
-  for i,log in enumerate(logs):
-    xlog[i] = log.x2[0]
-    ylog[i] = log.x3[0]
-  j = WellLogWarping.sortWells(xlog,ylog)
-  logt = list(logs)
-  for i,log in enumerate(logs):
-    logt[i] = logs[j[i]]
-  return logt
+  line = ifile.readline()
+  wdata = line.split('\t')
+  nl = len(wdata)
+  logs = zerofloat(nz-c,nl-1)
+  depth = zerodouble(nz-c)
+  depth[0] = float(wdata[0])
+  for l in range(1,nl):
+    logs[l-1][0] = float(wdata[l])
 
-def resample(logs,curve):
-  nlog = len(logs)
-  zs = zerofloat(0,nlog)
-  fs = zerofloat(0,nlog)
-  for i,log in enumerate(logs):
-    zs[i] = log.z
-    fs[i] = log.getCurve(curve)
-  zs = mul(zs,0.0003048) # ft to km
-  sz = wlw.getDepthSampling(zs,fs)
-  nz,dz,fz,lz = sz.count,sz.delta,sz.first,sz.last
-  #print "resample before: nz =",nz," dz =",dz," fz =",fz
-  dz = 0.001 # 1 m
-  nz = 1+int((lz-fz)/dz)
-  sz = Sampling(nz,dz,fz)
-  #print "resample  after: nz =",nz," dz =",dz," fz =",fz
-  fs = wlw.resampleLogs(sz,zs,fs)
-  return sz,fs
-
-def readLogSamples(set,type,smooth=0):
-  """ 
-  Reads log curves from the specified set that have the specified type.
-  set: "s" for shallow, "d" for deep, or "a" for all
-  type: "v" (velocity), "d" (density), "p" (porosity), or "g" (gamma)
-  smooth: half-width of Gaussian smoothing filter
-  Returns a tuple (f,x1,x2,x3) of lists of arrays of samples f(x1,x2,x3)
-  """
-  logs = getLogs(set,type)
-  fl,x1l,x2l,x3l = [],[],[],[]
-  for log in logs:
-    if smooth: 
-      log.smooth(smooth)
-    samples = log.getSamples(type)
-    if samples:
-      f,x1,x2,x3 = samples
-      fl.append(f)
-      x1l.append(x1)
-      x2l.append(x2)
-      x3l.append(x3)
-  return fl,x1l,x2l,x3l
-
-def getWellIntersections(set,type,x1):
-  fileName = _wellLogsDir+"tpw"+set[0]+".dat"
-  wdata = WellLog.Data.readBinary(fileName)
-  x2,x3 = wdata.getIntersections(type,x1)
-  return x2,x3
-
-def fgood(f):
-  n = len(f)
-  for i in range(n):
-    if f[i]!=-999.2500:
-      return i
-def lgood(f):
-  n = len(f)
-  for i in range(n):
-    if f[n-1-i]!=-999.2500:
-      return n-1-i
-
-def readImage():
-  fileName = _seismicLogsDir+"tpsz.dat"
-  n1,n2,n3 = 2762,357,161
-  x = zerofloat(n1,n2,n3)
-  ais = ArrayInputStream(fileName,ByteOrder.BIG_ENDIAN)
-  ais.readFloats(x)
-  ais.close()
-  return x
+  i = 1
+  start = False
+  while (start != True):
+    line = ifile.readline()
+    if line == "":
+      start = True
+    else:
+      wdata = line.split('\t')
+      depth[i] = float(wdata[0])
+      for l in range(1,nl):
+        logs[l-1][i] = float(wdata[l])
+      i += 1
+  ifile.close()
+  zs = Sampling(depth)
+  sz = Sampling(zs.count,zs.delta*0.001,zs.first)
+  return sz,logs
 
 def removeZeros(f):
   n = len(f)
